eb7262ad6b63ce8e45e16cd7f70a7a8129cf64a6
alkon134@gmail.com <Lelo132$>
2022-04-20 13:02:18 +0300

    pricing service test

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO PRICE (id, currency, price,vehicle_Id) VALUES (1, 'eur',11345,1l);
+INSERT INTO PRICE (id, currency, price,vehicle_Id) VALUES (2, 'usd',11345,2l);
+INSERT INTO PRICE (id, currency, price,vehicle_Id) VALUES (3, 'grp',11345,1l);
+INSERT INTO PRICE (id, currency, price,vehicle_Id) VALUES (4, 'eur',11345,2l);
+INSERT INTO PRICE (id, currency, price,vehicle_Id) VALUES (5, 'cfr',11345,1l);
